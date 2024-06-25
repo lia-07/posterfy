63da7eb18deae75e5a36f015ab0fdab87d8b7e1b
lia-07 <107384739+lia-07@users.noreply.github.com>
2024-06-25 21:43:29 +1200

    more modifications

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,17 +6,9 @@
 	import { onMount } from 'svelte';
 	import tilt from 'vanilla-tilt';
 
+	let hideRealPoster = true;
+
 	let tracklist = [
-		'Skinny',
-		'Lunch',
-		'Chihiro',
-		'Birds of a Feather BUT YUM',
-		'Wildflower',
-		'The Greatest',
-		"L'Amour de Ma Vie",
-		'The Diner',
-		'Bittersuite',
-		'Blue',
 		'Skinny',
 		'Lunch',
 		'Chihiro',
@@ -29,13 +21,24 @@
 		'Blue'
 	];
 
+	onMount(() => {
+		chunkedTracklist = [];
+		for (let i = 0; i < tracklist.length; i += 7) {
+			if (i <= 7 * 4) {
+				chunkedTracklist.push(tracklist.slice(i, i + 7));
+			}
+		}
+	});
+
+	let chunkedTracklist: any[] = [];
+
 	onMount(() =>
 		tilt.init(posterPreviewElement, {
-			max: 5,
-			speed: 1000,
+			max: 7,
 			reverse: true,
+			speed: 1500,
 			glare: true,
-			'max-glare': 0.6
+			'max-glare': 0.5
 		})
 	);
 
@@ -56,27 +59,27 @@
 	}
 </script>
 
-<div class="flex h-full w-full items-center justify-center">
+<div class="flex h-full w-full items-center justify-center p-8">
 	<div
 		bind:this={posterPreviewElement}
-		class="asepct-[1/1.41] w-[30rem] overflow-clip rounded-md drop-shadow-xl"
+		class="asepct-[1/1.41] h-full overflow-clip rounded-md drop-shadow-xl"
 	>
 		{#if posterPreview}
-			<img class="w-full" src={posterPreview} alt="Preview" />
+			<img class="h-full" src={posterPreview} alt="Preview" />
 		{/if}
 	</div>
 </div>
-<div class="pointer-events-none absolute -z-50 opacity-0">
+<div class={hideRealPoster ? 'pointer-events-none absolute -top-[297mm] -z-50' : ''}>
 	<div
 		bind:this={poster}
-		class=" flex h-[297mm] w-[210mm] flex-col bg-[#FFFCF0] px-[16mm] py-[12mm] font-instrument drop-shadow"
+		class=" flex h-[297mm] w-[210mm] flex-col bg-[#FFFCF0] px-[16mm] py-[12mm] font-instrument"
 	>
 		<img src={hmhas} class="mb-[4mm] aspect-square w-full" alt="" />
-		<span class="ptxt-title line-clamp-1 overflow-ellipsis font-bold italic"
+		<span class="ptxt-title text-ellipses line-clamp-1 pl-[0.75mm] font-bold italic"
 			>HIT ME HARD AND SOFT</span
 		>
-		<div class="flex items-baseline justify-between">
-			<div class="flex h-fit items-baseline gap-[4mm] pt-[2mm]">
+		<div class="flex items-end justify-between">
+			<div class="flex h-fit items-end gap-[4mm] pt-[2mm]">
 				<div class="flex h-[8mm] gap-[1mm]">
 					<div class="aspect-square h-full bg-[#041222]"></div>
 					<div class="aspect-square h-full bg-[#032850]"></div>
@@ -91,34 +94,31 @@
 			<span class="text-[6mm] italic opacity-80">14 songs, 44m</span>
 		</div>
 
-		<div class="grid grid-cols-3 gap-[4mm] pt-[4mm] font-sans text-[5mm] leading-[6mm] opacity-60">
-			<div
-				class="[&>*]:overflow-ellipsisline-clamp-1 col-span-1 overflow-clip [&>*]:line-clamp-1 [&>*]:overflow-ellipsis [&>*]:leading-[]"
-			>
-				{#each tracklist.slice(0, 6) as track, i}
-					<p>{i + 1}. {track}</p>
-				{/each}
-			</div>
-			<div class="col-span-1 overflow-clip [&>*]:line-clamp-1 [&>*]:overflow-ellipsis">
-				{#each tracklist.slice(6, 12) as track, i}
-					<p>{i + 6 + 1}. {track}</p>
-				{/each}
-			</div>
-			<div class="col-span-1 overflow-clip [&>*]:line-clamp-1 [&>*]:overflow-ellipsis">
-				{#each tracklist.slice(12, 18) as track, i}
-					{#if i === 5 && tracklist[19]}
-						<!-- content here -->
-						<span class="italic">...{tracklist.length - 17} more</span>
-					{:else}
-						<p>{i + 12 + 1}. {track}</p>
-					{/if}
-				{/each}
-			</div>
+		<div class="flex gap-[2mm] pt-[5mm] font-sans text-[5mm] leading-[6mm] opacity-60">
+			{#each chunkedTracklist as tracks, i}
+				<div class="flex-1">
+					{#each tracks as track, j}
+						<div
+							class="line-clamp-1 flex-1 overflow-clip [&>*]:line-clamp-1 [&>*]:overflow-ellipsis"
+						>
+							{#if i === 4 && j === 6 && tracklist[28]}
+								<!-- content here -->
+								<span class="italic">...{tracklist.length - 7 * 5} more</span>
+							{:else}
+								<p>{j + i * 7 + 1}. {track}</p>
+							{/if}
+						</div>
+					{/each}
+				</div>
+			{/each}
 		</div>
 		<div class=" flex h-max flex-1 items-end justify-between">
-			<span class="line-clamp-1 max-w-[100mm] text-ellipsis font-sans text-[4mm] opacity-70"
-				>© 2024 Darkroom/Interscope Records</span
+			<div
+				class="max-w-[100mm] font-sans text-[3.5mm] leading-[3.5mm] opacity-60 [&>*]:line-clamp-1 [&>*]:text-ellipsis"
 			>
+				<span class="">© 2024 Darkroom/Interscope Records.</span>
+				<span>Content sourced from Spotify.</span>
+			</div>
 			<img src={hmhasCode} class="w-[45mm]" alt="" />
 		</div>
 	</div>
